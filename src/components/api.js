479a21027ca25f6dda359003cc303704449849e8
yagyasoni <yagyasony@gmail.com>
2025-12-16 15:24:41 +0530

    deployed to AWS

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 1. Create a custom Axios instance
 const api = axios.create({
   // Setting a relative base URL for cleaner endpoint calls in components
-  baseURL: 'https://service-app-backend-1.onrender.com/api', 
+  baseURL: 'http://3.7.66.48:5000/api', 
   headers: {
     'Content-Type': 'application/json',
   },
